8770f39b119752d4f60908d7e558e5cb159fa984
clevergrant <clevergrant@gmail.com>
2019-06-13 16:47:25 -0600

    finished project 11 - made progress bar set on video load, created window-scoped clicked indication variable

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,63 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+let clicked = false
+
+/**
+ * Functions
+ */
+
+function togglePlay() {
+	video.paused ? video.play() : video.pause()
+}
+
+function updateButton() {
+	const icon = this.paused ? '►' : '❚❚'
+	toggle.textContent = icon
+}
+
+function skip() {
+	video.currentTime += parseFloat(this.dataset.skip)
+}
+
+function handleRangeUpdate() {
+	if (clicked) video[this.name] = this.value
+}
+
+function handleProgress() {
+	const percent = video.currentTime / video.duration * 100
+	progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e) {
+	if (clicked) video.currentTime = e.offsetX / progress.offsetWidth * video.duration
+}
+
+/**
+ * Event Listeners
+ */
+
+video.addEventListener('click', togglePlay)
+video.addEventListener('play', updateButton)
+video.addEventListener('pause', updateButton)
+video.addEventListener('timeupdate', handleProgress)
+
+toggle.addEventListener('click', togglePlay)
+
+skipButtons.forEach(button => button.addEventListener('click', skip))
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
+
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousemove', scrub)
+
+window.addEventListener('mousedown', () => clicked = true)
+window.addEventListener('mouseup', () => clicked = false)
+
+video.onloadeddata = handleProgress
